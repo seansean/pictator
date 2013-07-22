9a07cda0ed4c13ec02d120e89e51419f98cdf64c
seansean <ssefcik@gmail.com>
2013-07-22 13:44:40 -0400

    Pause rotation on interaction.
    Now pauses when the mouse is hovering within the slideshow.

--- src/jquery.pictator.js
+++ src/jquery.pictator.js
@@ -11,23 +11,29 @@
 	var name = 'pictator';
 
 	function Pictator(el, opts) {
+		// DOM Elements
 		this.$el 		= $(el);
 		this.$slides 	= null;
 
+		// Properties
 		this.defaults = {
 			slideDuration: 5,
 			transitionDuration: .5,
 			zIndex: 2
 		};
 
-		this.firstRun 	= true;
-		this.curPic 		= 0;
-
 		var meta 		= this.$el.data(name + '-opts');
 		this.opts 		= $.extend(this.defaults, opts, meta);
 
-		this.$el.data(name, this);
+		// Variables
+		this.firstRun 			= true;
+		this.paused 			= false;
+		this.curPic 			= 0;
+		this.slideDuration 		= this.opts.slideDuration * 1000;
+		this.transitionDuration = this.opts.transitionDuration * 1000;
+		this.slideInterval 		= null;
 
+		this.$el.data(name, this);
 		this.init();
 	}
 
@@ -39,6 +45,19 @@
 		self.$el.append(slidesDiv);
 		self.$slides 	= self.$el.find('.pictator-slides');
 
+		// Set up mouseenter pause/play
+		self.$el.hover(
+			function(){
+				self.paused = true;
+				clearTimeout(self.slideInterval);
+			},
+			function(){
+				self.paused = false;
+				self.startRotator(self);
+			}
+		);
+
+		// Start rotation
 		self.parsePics(this, self.opts.picURLs);
 	};
 
@@ -50,50 +69,60 @@
 			var z 		= (index == 0 ? self.opts.zIndex : self.opts.zIndex - 1);
 			var d 		= (index < 2 ? 'block' : 'none');
 			var pic 	= $('<div class="pictator-photo photo-'+ index +'"></div>')
-					//.hide()
 					.css({'position':'absolute', 'opacity':'1', 'z-index':z, 'display':d})
 					.append('<img src="' + value + '" style="position:relative">');
 			self.$slides.append(pic);
 		});
 		
-		self.startRotator(self);
+		// Check if player is paused, if not, loop
+		if (!self.paused) {
+			self.startRotator(self);
+		}
 	};
 
 	// Start rotation
 	Pictator.prototype.startRotator = function(self) {
-		var self = this;
-
-		// Fade in next slide
-		self.$slides.children('div').eq(self.nextPic).fadeIn();
-
-		// Fade down current slide
-		self.$slides.children('div').eq(self.curPic)
-		  .css({'z-index': self.opts.zIndex})
-		  .delay(self.opts.slideDuration * 1000)
-		  .fadeOut(self.opts.transitionDuration * 1000, function(){_fadeComplete(self, this)});
-
+		self.slideInterval = setTimeout(function(){_fadeStart(self);}, self.slideDuration);
 	};
+
+	function _fadeStart(self) {
+		if (!self.paused) {
+			// Stop timer
+			clearTimeout(self.slideInterval);
+
+			// Make next slide visible
+			self.$slides.children('div').eq(self.nextPic).show();
+
+			// Fade down current slide
+			self.$slides.children('div').eq(self.curPic)
+			  .css({'z-index': self.opts.zIndex})
+			  //.delay(self.slideDuration)
+			  .fadeOut(self.transitionDuration, function(){_fadeComplete(self, this)});
+		}
+	}
 
 	// Fires on crossfade completion
 	function _fadeComplete(self, curSlide) {
-	  	// Set z-index
-		$(curSlide).css({'z-index':self.opts.zIndex - 1})
+		if (!self.paused) {
+		  	// Set z-index
+			$(curSlide).css({'z-index':self.opts.zIndex - 1})
 
-		// Update curPic and nextPic
-		if (self.curPic < self.opts.picURLs.length - 1) {
-			self.curPic++
-			if (self.curPic == self.opts.picURLs.length - 1) {
-				self.nextPic = 0;
+			// Update curPic and nextPic
+			if (self.curPic < self.opts.picURLs.length - 1) {
+				self.curPic++
+				if (self.curPic == self.opts.picURLs.length - 1) {
+					self.nextPic = 0;
+				} else {
+					self.nextPic = self.curPic + 1
+				}
 			} else {
+				self.curPic = 0;
 				self.nextPic = self.curPic + 1
 			}
-		} else {
-			self.curPic = 0;
-			self.nextPic = self.curPic + 1
-		}
 
-		// Trigger next transition
-		self.startRotator(self);
+			// Trigger next transition
+			self.startRotator(self);
+		}
 	}
 	
 
